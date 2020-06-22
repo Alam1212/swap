14869490c2a7837f8344efa1af49ee7e3e519a4b
Alam1212 <61057660+Alam1212@users.noreply.github.com>
2020-06-22 08:35:53 -0400

    swap

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+
+public class Main{
+    //Write a Java program to swap two variables
+
+    public static void main(String[] strings) {
+        int a, b ,temp;
+        a = 15;
+        b = 27;
+        System.out.println("before" + " " + a + "," + b);
+        System.out.println("after" + " " + b + "," + a);
+
+        }
+
+
+    }
+
+
